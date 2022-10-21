b806ea8b8d3323369f39765e9da902cdfc20392a
Piotr <pioch0408@gmail.com>
2022-10-21 14:57:56 +0200

    v0.2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$(document).on("turbo:load", () => {
+    console.log("turbo");
+}) 
