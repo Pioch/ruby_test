1e0a0e30dd7eaead1c0edd2610c8a78f80bed1d7
Piotr <pioch0408@gmail.com>
2022-10-24 13:51:59 +0200

    v0.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_104003) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_112514) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_104003) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
