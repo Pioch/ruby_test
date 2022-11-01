3b8d13245602675be93d59e494806b13417f100f
Piotr <pioch0408@gmail.com>
2022-11-01 13:57:19 +0100

    v0.6

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends or /friends.json
   def index
@@ -13,7 +14,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new #zmiana
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -22,7 +24,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    #@friend = Friend.new(friend_params) #zmiana
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -58,6 +61,12 @@ class FriendsController < ApplicationController
     end
   end
 
+  #sprawdzenie zalogowanego użytkownika
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Brak uprawnień do edycji" if @friend.nil?
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  #get 'home/index' #ścieżka do strony
-  root 'home#index' #ustawienie strony jako startowa
+  get 'home/index' #ścieżka do strony
+  root 'friends#index' #ustawienie strony jako startowa
   get 'home/about'
   get 'home/friends'
   
